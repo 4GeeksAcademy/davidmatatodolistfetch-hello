6ce9c9466521cf57aa0561f32c493a53627a256e
davidmata97-cell <daviddemata97@gmail.com>
2025-01-15 10:38:08 +0000

    First commit

---
+++ src/js/component/form.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Form  () {
+    return(
+    <form>
+        <div class="mb-3">   
+            <input class="form-control" type="text" id="formFile"/>
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="text" id="formFile"/>
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="text" id="formFile"/>
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="text" id="formFile"/>
+        </div>
+        <div>
+            <input class="form-control" type="text" id="formFile"/>
+        </div>
+    </form>
+
+    );
+};
+export default Form;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,70 @@
-import React from "react";
+
+import Form from "./form";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from 'react';
+
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([
+		{ done: false, title: 'Study software develop', id: Math.random() * 10 },
+		{ done: false, title: 'do the homework from the academy', id: Math.random() * 10 },
+		{ done: false, title: 'do any kinf of sport', id: Math.random() * 10 },
+		{ done: false, title: 'Walk the dog', id: Math.random() * 10 }
+	  ]);
+
+	  const [taskInput, setTaskInput] = useState('');
+
+  	const handleFormSubmit = (e) => {
+    	e.preventDefault();
+    	setTodos([
+     	 ...todos,
+      	{ title: taskInput, done: false, id: Math.random() * 10 }
+    	]);
+    	setTaskInput('');
+  	};
+
+	const deleteTask = (taskId) => {
+		setTodos(todos.filter(task => task.id !== taskId));
+	};
+
+	const tasksToRender = todos.map(task => (
+		<li key={task.id}>
+		<div className="view">
+			<label>{task.title}</label>
+			<button className="destroy" onClick={() => deleteTask(task.id)}></button>
+		</div>
+		</li>
+  		));
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+				<h1 className="text-center mt-5"></h1>
+				<section className="todoapp">
+			<header className="header">
+				<h1>todos</h1>
+				<form onSubmit={handleFormSubmit}>
+				<input
+					autoFocus={true}
+					className="new-todo"
+					placeholder="What needs to be done?"
+					value={taskInput}
+					onChange={(e) => setTaskInput(e.target.value)}
+				/>
+				</form>
+			</header>
+			<section className="main">
+				<ul className="todo-list">
+				{tasksToRender}
+				</ul>
+			</section>
+			<footer className="footer">
+				<span className="todo-count">
+				<strong>{todos.filter(task => !task.done).length}</strong> item left
+				</span>
+			</footer>
+			</section>
 		</div>
 	);
 };
