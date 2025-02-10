2afba04f3ad482a39b5db9372b38ddbe64d7cbcd
davidmata97-cell <daviddemata97@gmail.com>
2025-02-10 10:10:55 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,72 +1,156 @@
 
-import Form from "./form";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import React, { useState } from 'react';
-
-
-//create your first component
 const Home = () => {
-	const [todos, setTodos] = useState([
-		{ done: false, title: 'Study software develop', id: Math.random() * 10 },
-		{ done: false, title: 'do the homework from the academy', id: Math.random() * 10 },
-		{ done: false, title: 'do any kinf of sport', id: Math.random() * 10 },
-		{ done: false, title: 'Walk the dog', id: Math.random() * 10 }
-	  ]);
+	const [input, setInput] = useState({ label: "", done: false });
+	const [addTodo, setAddTodo] = useState([]);
+	const [deleteTask, setdeleteTask] = useState(null);
+	const [firstRender, setFirstRender] = useState(false);
+	
 
-	  const [taskInput, setTaskInput] = useState('');
+	useEffect(() => {
+		getList();
+	}, []);
 
-  	const handleFormSubmit = (e) => {
-    	e.preventDefault();
-    	setTodos([
-     	 ...todos,
-      	{ title: taskInput, done: false, id: Math.random() * 10 }
-    	]);
-    	setTaskInput('');
-  	};
+	useEffect(() => {
+		if (firstRender) {
+			updateList();
+		}
+	}, [addTodo]);
 
-	const deleteTask = (taskId) => {
-		setTodos(todos.filter(task => task.id !== taskId));
+	const validateInputEnter = (e) => {
+		const labels = addTodo.map((e) => e.label);
+		const newInput = e.target.value.trim();
+		console.log(e);
+		if (e.key == "Enter") {
+			if (newInput != "" && !labels.includes(newInput)) {
+				setAddTodo([...addTodo, input]);
+				e.target.value = "";
+			} else {
+				alert("No se pueden repetir tareas o introducir tareas vacias");
+			}
+		}
 	};
 
-	const tasksToRender = todos.map(task => (
-		<li key={task.id}>
-		<div className="view">
-			<label>{task.title}</label>
-			<button className="destroy" onClick={() => deleteTask(task.id)}></button>
-		</div>
-		</li>
-  		));
+	const validateInputClick = (e) => {
+		const labels = addTodo.map((e) => e.label);
+		const newInput = input.label.trim();
+		console.log(e);
+
+		if (e.type == "click") {
+			if (newInput != "" && !labels.includes(newInput)) {
+				setAddTodo([...addTodo, input]);
+				e.target.value = "";
+			} else {
+				alert("No se pueden repetir tareas o introducir tareas vacias");
+			}
+		}
+	};
+
+	const getList = async () => {
+		const Response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/davidcell97",
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const result = await Response.json();
+		setAddTodo(result);
+		setFirstRender(true);
+	};
+
+	const updateList = async () => {
+		await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/davidcell97",
+			{
+				method: "PUT", // or 'POST'
+				body: JSON.stringify(addTodo), 
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	};
 
 	return (
-		<div className="text-center">
-				<h1 className="text-center mt-5"></h1>
-				<section className="todoapp">
-			<header className="header">
-				<h1>todos</h1>
-				<form onSubmit={handleFormSubmit}>
-				<input
-					autoFocus={true}
-					className="new-todo"
-					placeholder="What needs to be done?"
-					value={taskInput}
-					onChange={(e) => setTaskInput(e.target.value)}
-				/>
-				</form>
-			</header>
-			<section className="main">
-				<ul className="todo-list">
-				{tasksToRender}
-				</ul>
-			</section>
-			<footer className="footer">
-				<span className="todo-count">
-				<strong>{todos.filter(task => !task.done).length}</strong> item left
-				</span>
-			</footer>
-			</section>
+		<div className="container">
+			<div className="mb-3 w-75 mx-auto">
+				<label htmlFor="exampleInputEmail1" className="form-label">
+					<h2 className="h2">To2</h2>
+				</label>
+				<div className="d-flex justify-content-start">
+					<div className="w-75">
+						{" "}
+						<input
+							type="text"
+							className="form-control"
+							value={input.label}
+							onChange={(e) => {
+								setInput({ ...input, label: e.target.value });
+							}}
+							onKeyDown={(e) => {
+								validateInputEnter(e);
+							}}
+						/>
+					</div>
+					<div>
+						{" "}
+						<button
+							type="button"
+							className="btn btn-light"
+							value={input.label}
+							onClick={(e) => {
+								validateInputClick(e);
+							}}>
+							Add
+						</button>
+					</div>
+				</div>
+				{addTodo.map((e, i) => {
+					if (i != 0) {
+						return (
+							<div
+								key={i}
+								className="d-flex justify-content-between"
+								onMouseEnter={() => setdeleteTask(i)}
+								onMouseLeave={() => setdeleteTask(null)}>
+								<div className="form-control">{e.label}</div>
+								<div>
+									{deleteTask == i && (
+										<button
+											type="button"
+											className="btn-close text-end"
+											aria-label="Close"
+											onClick={() => {
+												setAddTodo(
+													addTodo.filter(
+														(v, index) =>
+															index !== i
+													)
+												);
+												
+											}}></button>
+									)}
+								</div>
+							</div>
+						);
+					}
+				})}
+				<div>
+					{" "}
+					<p className="h6">
+						{" "}
+						{addTodo.length > 1
+							? addTodo.length - 1 + " items left"
+							: "add something to-do"}
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
